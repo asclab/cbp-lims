416cc026bd141e2b30642447cb31f34a7ecefb70
Alex Lee <tree101@stanford.edu>
2015-12-08 22:04:22 -0800

    join and minor css for form

--- src/cbplims/groups/__init__.py
+++ src/cbplims/groups/__init__.py
@@ -4,22 +4,15 @@ from flask import g
 
 
 
-Project = namedtuple('Group', 'id project_id name is_admin is_view project_name')
+Project = namedtuple('Group', 'id name is_admin is_view project_name')
 
 def avail_groups():
     cur = g.dbconn.cursor()
     groups = []
-    sql = "SELECT id, project_id, name, is_admin, is_view FROM groups;"
+    sql = "SELECT a.id, a.name, a.is_admin, a.is_view, b.name FROM groups a LEFT JOIN projects b ON a.project_id = b.id"
     cur.execute(sql)
 
     for record in cur:
-        sql2 = "SELECT name FROM projects WHERE id = %s;"
-        cur2 = g.dbconn.cursor()
-        cur2.execute(sql2,(record[1],))
-        temp = cur2.fetchone()
-        project_name = temp[0]
-        cur2.close()
-        record = record + (project_name,)
         groups.append(Project(*record))
     cur.close()    
     return groups
