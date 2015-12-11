d7f76f2e00e67a596ea6d8fd912374d27edfd33b
Marcus Breese <mbreese@stanford.edu>
2015-12-10 19:26:50 -0500

    pep8

--- src/cbplims/users/__init__.py
+++ src/cbplims/users/__init__.py
@@ -4,8 +4,6 @@ import cbplims.auth
 from flask import g
 from collections import namedtuple
 
-
-
 User = namedtuple('user', 'id username fullname is_global_admin')
 
 
@@ -15,7 +13,6 @@ def get_users():
     cur.execute(c)
     users = []
     for record in cur:
-        app.logger.debug(str(record))
         users.append(User(*record))
     cur.close()
 
@@ -37,24 +34,25 @@ def get_user(uid):
     return user
 
 
-def add_user(full_name,username,is_admin,pwd):
+def add_user(full_name, username, is_admin, pwd):
     cur = g.dbconn.cursor()
     sql = 'INSERT INTO users (id,username, fullname, password, is_global_admin) VALUES (DEFAULT,%s,%s,%s,%s)'
     pwd = cbplims.auth.auth_pbkdf2.generate_new_password_string(pwd)
+
     # normally I use "try" and then a rollback if it does not work.
     # don't know where the loggers are?
-    # app.logger.audti? 
-    cur.execute(sql, (username,full_name,pwd,is_admin) )
+    # app.logger.audti?
+
+    cur.execute(sql, (username, full_name, pwd, is_admin))
     g.dbconn.commit()
     cur.close()
     return True
+
 
 def del_user(id):
     cur = g.dbconn.cursor()
     sql = 'DELETE FROM users WHERE id = %s '
-    cur.execute(sql, (id,) )
+    cur.execute(sql, (id,))
     g.dbconn.commit()
     cur.close()
     return True
-
-
